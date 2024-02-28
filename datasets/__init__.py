00aa43c18752c50bfe8c6eed0c7425beca9caaa1
mazitong <1053872079@qq.com>
2024-02-28 17:44:40 +0800

    mid1

--- datasets/__init__.py
+++
@@ -1,2 +0,0 @@
-from .data_helper import load_ft
-from .visual_data import load_feature_construct_H
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 
 import torch
 import numpy as np
+import copy
 from time import time
 import torch.optim as optim
-from models import DHGCF
-from models import HGNN
+from models import DHGNN
 from utils import parser
 from utils import loaddata_link
 from operator import itemgetter
@@ -24,7 +24,7 @@ def test(model, users_to_test, num,args,test_set,neg_test_dic):
     if True:
         u_g_embeddings= model( drop_flag=False)
         rate=model.rating(u_g_embeddings,u_g_embeddings)
-        rate = torch.matmul(u_g_embeddings, u_g_embeddings.t())
+
 
         rate_batch = torch.sigmoid(rate)
         pos_n=0
@@ -71,29 +71,7 @@ def early_stopping(log_value, best_value, stopping_step, expected_order='acc', f
         should_stop = False
     return best_value, stopping_step, should_stop
 
-
-if __name__ == '__main__':
-
-    args = parser.parse_args()
-    args.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    args.node_dropout = eval(args.node_dropout)
-    args.mess_dropout = eval(args.mess_dropout)
-    args.alpha=0.1
-
-
-    L, nums, edge_dic, train_dic, test_dic, neg_test_dic, n_train, n_test ,user_item_matrix,_= loaddata_link.readdata(args)
-
-    user_item_matrix = torch.tensor((user_item_matrix))
-
-    test_neg_dict,_ = loaddata_link.sample_neg(test_dic, edge_dic, nums)
-    train_neg_dict,u_i_matrix = loaddata_link.sample_neg(train_dic, edge_dic, nums)
-    mask = user_item_matrix > 0
-    neg_mask = u_i_matrix == -1
-
-
-    model = DHGCF(nums, L, args).to(args.device)
-
-
+def train_func(model,args,mask):
     t0 = time()
 
     """
@@ -106,32 +84,24 @@ if __name__ == '__main__':
                                                milestones=args.milestones,
                                                gamma=args.gamma)
 
-
     best_auc, best_ap = 0, 0
-    lossdic=[]
-    times=0
+    test_neg_dict, _ = loaddata_link.sample_neg(test_dic, edge_dic, nums)
+    users_to_test = list(test_dic.keys())  # test中的uid，全部的user
+
 
     for epoch in range(args.epoch):
         model.train()
         t1 = time()
-        loss, mf_loss, emb_loss = 0., 0., 0.
-        BCEloss = torch.nn.BCELoss()
+        epoch_loss, mf_loss, emb_loss = 0., 0., 0.
 
         u_g_embeddings = model(drop_flag=args.node_dropout_flag)
-
-
-
         rate_batch = torch.matmul(u_g_embeddings, u_g_embeddings.t())
 
-        pred_p = mask * rate_batch
-
         train_neg_dict, u_i_matrix = loaddata_link.sample_neg(train_dic, edge_dic, nums)
-
         neg_mask = u_i_matrix == -1
-        neg_dict = train_neg_dict
 
-        pred_n = neg_mask * rate_batch
-
+        pred_p = torch.mul(mask, rate_batch)
+        pred_n = torch.mul(neg_mask ,rate_batch)
 
         def flaten(a):
             a = a.flatten()
@@ -139,79 +109,102 @@ if __name__ == '__main__':
             b = torch.index_select(a, dim=0, index=nonzero.squeeze())
             return b
 
-
         pred_p = flaten(pred_p)
         pred_n = flaten(pred_n)
 
         # pred_n calculate 0
-        if pred_n.size(0) < n_train:
-            pred_n = torch.cat([pred_n, torch.zeros(n_train - pred_n.size(0))], dim=0)
+        if pred_n.shape<pred_p.shape:
+            pred_n=torch.cat([pred_n,torch.zeros(pred_p.size(0)-pred_n.size(0))],dim=0)
 
+        # 1 if an edge exists, else 0
         label = torch.cat((torch.ones(n_train), torch.zeros(n_train)), dim=0)
 
         pred = (torch.cat((pred_p, pred_n), dim=0))
 
         decay = eval(args.regs)[0]
         maxi = torch.nn.LogSigmoid()(pred_p - pred_n)
-
         mf_loss = -1 * torch.mean(maxi)
-        posloss = -torch.log(torch.sigmoid(pred_p) + decay).mean()
-        negloss = -torch.log(1 - torch.sigmoid(pred_n) + decay).mean()
 
         regularizer = (torch.norm(u_g_embeddings) ** 2) / 2
         emb_loss = decay * regularizer / nums
-        # mf_loss=BCEloss(torch.sigmoid(pred),label)
-        batch_loss = mf_loss + emb_loss
+
+        epoch_loss = mf_loss + emb_loss
+
         optimizer.zero_grad()
-        print(time()-t1)
-
-        batch_loss.backward()
-
+        epoch_loss.backward()
         optimizer.step()
         schedular.step()
 
         train_auc = roc_auc_score(label, torch.sigmoid(pred).detach().numpy())
         train_ap = average_precision_score(label, torch.sigmoid(pred).detach().numpy())
 
-        loss += batch_loss
-
-        users_to_test = list(test_dic.keys())  # test中的uid，全部的user
-
+        # validation
         model.eval()
-        test_auc, test_ap = test(model, users_to_test, nums, args, test_dic, test_neg_dict)
-        if best_auc < test_auc:
-            best_auc = test_auc
-            best_ap = test_ap
-        perf_str = 'Epoch %d [%.1fs]: loss==[%.5f=%.5f + %.5f],auc=[%.5f],ap=[%.5f]' % (
-            epoch, time() - t1, loss, mf_loss, emb_loss, train_auc, train_ap)
-        print(perf_str)
-        times+=time()-t1
-        if (epoch + 1) % 10 == 0:
 
-            print('current: test_auc=[%.5f], test_ap=[%.5f])' % (test_auc, test_ap))
-            print('best: best_auc=[%.5f], best_ap=[%.5f]' % (best_auc, best_ap))
 
-        best_auc, stopping_step, should_stop = early_stopping(test_auc, best_auc,
+
+        val_auc, val_ap = test(model, users_to_test, nums, args, test_dic, test_neg_dict)
+
+        if best_auc < val_auc:
+            best_auc = val_auc
+            best_ap = val_ap
+            best_model_wts = copy.deepcopy(model.state_dict())
+
+        if (epoch + 1) % 100 == 0:
+            perf_str = 'Epoch %d [%.1fs]: loss==[%.5f=%.5f + %.5f],auc=[%.5f],ap=[%.5f]' % (
+                epoch, time() - t1, epoch_loss, mf_loss, emb_loss, train_auc, train_ap)
+            print(perf_str)
+            print('current: val_auc=[%.5f], val_ap=[%.5f])' % (val_auc, val_ap))
+            print('best: val_auc=[%.5f], val_ap=[%.5f]' % (best_auc, best_ap))
+
+        best_auc, stopping_step, should_stop = early_stopping(val_auc, best_auc,
                                                               stopping_step, expected_order='acc', flag_step=200)
 
         # *********************************************************
         # # early stopping when cur_best_pre_0 is decreasing for ten successive steps.
-
-        lossdic.append(test_auc)
-        if epoch==800:
-            print('times:', times, 'times/epoch', times / (epoch + 1))
-            break
         if should_stop == True:
             break
-    t1 = time()
-    print('training time: ',t1-t0)
-    users_to_test = list(test_dic.keys())  # test中的uid，全部的user
+    t2 = time()
 
-    test_auc, test_ap = test(model, users_to_test, nums, args, test_dic,test_neg_dict)
-    print('current: test_auc=[%.5f], test_ap=[%.5f])' % (test_auc, test_ap))
-    print('best: best_auc=[%.5f], best_ap=[%.5f]' % (best_auc, best_ap))
+
+    model.load_state_dict(best_model_wts)
+
+
+    test_auc, test_ap = test(model, users_to_test, nums, args, test_dic, test_neg_dict)
+
+    print('training time: ', t2 - t0)
+    print('test: test_auc=[%.5f], test_ap=[%.5f])' % (test_auc, test_ap))
+    #print('best: best_auc=[%.5f], best_ap=[%.5f]' % (best_auc, best_ap))
     t3 = time()
-    np.savetxt('./data/'+args.dataset+args.split+'_acc.txt',lossdic)
+    return best_auc,best_ap
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    args.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    args.node_dropout = eval(args.node_dropout)
+    args.mess_dropout = eval(args.mess_dropout)
+    changestr=str(args.alpha)
+    best=[]
+    for i in range(10):
+        args.split='/split'+str(i)
+        print('-------------------------------------------------------')
+        print('start! dataset:'+args.dataset+'  '+args.split)
+        L, nums, edge_dic, train_dic, test_dic, neg_test_dic, n_train, n_test, user_item_matrix,tml= loaddata_link.readdata1(
+            args)
+
+        user_item_matrix = torch.tensor((user_item_matrix))
+        mask = user_item_matrix == 1
+
+
+
+        model = DHGNN(nums, L, args).to(args.device)
+        best_auc,best_ac=train_func(model, args, mask)
+        best.append([best_auc,best_ac])
+
+
+
+
+
 
 
 
